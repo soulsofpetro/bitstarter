da6cab8eb3b6dd535787c9d7cb5a646ea9a7aff8
Petros Kapakos <soulsofpetro@web.de>
2013-07-10 23:26:30 +0000

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var readFile = "index.html";
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(getFileText());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var getFileText = function(){
+    var buffer = fs.readFileSync(readFile);
+    return buffer.toString("utf8", 0, buffer.length);
+} 
